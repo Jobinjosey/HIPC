a7e59b52b38b91ecae1eab6719965859d874f952
Jobinjosey <71965075+Jobinjosey@users.noreply.github.com>
2021-05-12 23:04:00 +0530

    Create Question_no_5.py

---
+++ Assignment/chapter2/Question_no_5.py
@@ -0,0 +1,42 @@
+# successive over-relaxation (SOR)
+
+# Defining equations to be solved
+# in diagonally dominant form
+f1 = lambda x,y,z: (4.5-2*y)/3
+f2 = lambda x,y,z: (5-2*x+z)/3
+f3 = lambda x,y,z: (-0.5+y)/2
+
+# Initial setup
+x0 = 0
+y0 = 0
+z0 = 0
+count = 1
+
+# Reading tolerable error
+e = float(input('Enter tolerable error: '))
+
+# Reading relaxation factor
+w = float(input("Enter relaxation factor: "))
+
+# Implementation of successive over-relaxation
+print('\nCount\tx\ty\tz\n')
+
+condition = True
+
+while condition:
+    x1 = (1-w) * x0 + w * f1(x0,y0,z0)
+    y1 = (1-w) * y0 + w * f2(x1,y0,z0)
+    z1 = (1-w) * z0 + w * f3(x1,y1,z0)
+    print('%d\t%0.4f\t%0.4f\t%0.4f\n' %(count, x1,y1,z1))
+    e1 = abs(x0-x1);
+    e2 = abs(y0-y1);
+    e3 = abs(z0-z1);
+    
+    count += 1
+    x0 = x1
+    y0 = y1
+    z0 = z1
+    
+    condition = e1>e and e2>e and e3>e
+
+print('\nSolution: x = %0.3f, y = %0.3f and z = %0.3f\n'% (x1,y1,z1))
